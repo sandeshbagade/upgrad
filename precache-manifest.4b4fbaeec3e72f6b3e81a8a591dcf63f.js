e7ed7e557b0cade3044a0638a94e29545ed028fe
sandeshbagade <sandeshbagade25@gmail.com>
2019-03-05 14:59:15 +0530

    Updates

---
+++ precache-manifest.4b4fbaeec3e72f6b3e81a8a591dcf63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f91075ae30f0280224",
+    "url": "/upgrad/static/css/main.f4baeaa4.chunk.css"
+  },
+  {
+    "revision": "45f91075ae30f0280224",
+    "url": "/upgrad/static/js/main.45f91075.chunk.js"
+  },
+  {
+    "revision": "daaf546f5bda08ee30ee",
+    "url": "/upgrad/static/js/1.daaf546f.chunk.js"
+  },
+  {
+    "revision": "d76fadd3fcc383527bb9",
+    "url": "/upgrad/static/js/runtime~main.d76fadd3.js"
+  },
+  {
+    "revision": "8cfe1c850a9153c2c6275ed038587a82",
+    "url": "/upgrad/index.html"
+  }
+];
